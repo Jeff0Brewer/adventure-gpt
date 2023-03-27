b7c655ca991e9f28921b8eaeebd2b5877ca1cca7
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-03-27 12:09:10 -0600

    init story generation

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -1,10 +1,32 @@
-import React, { FC, useRef, useState } from 'react'
+import React, { FC, useRef, useState, useEffect } from 'react'
 import type { ChatCompletionRequestMessage as Message } from 'openai'
+import PROMPT from '@/lib/prompt'
+import { postBody } from '@/lib/fetch'
 
 const Game: FC = () => {
     const inputRef = useRef<HTMLInputElement>(null)
     const outputRef = useRef<HTMLParagraphElement>(null)
-    const [messages, setMessages] = useState<Array<Message>>([])
+    const [messages, setMessages] = useState<Array<Message>>([
+        { role: 'user', content: PROMPT }
+    ])
+
+    useEffect(() => {
+        getCompletion(messages)
+    }, [])
+
+    const getCompletion = async (messages: Array<Message>): Promise<void> => {
+        const completion = await fetch('/api/chat-complete', postBody({ messages }))
+        if (completion.status === 200) {
+            const { message } = await completion.json()
+            setMessages([
+                ...messages,
+                { role: 'assistant', content: message }
+            ])
+        } else {
+            const { message } = await completion.json()
+            console.error(`Chat complete error: ${message}`)
+        }
+    }
 
     const getLastResponse = (messages: Array<Message>): string => {
         const responses = messages.filter(msg => msg.role === 'assistant')
