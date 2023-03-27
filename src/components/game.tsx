bc6a7f6672694879e8f8e1d1ada93603aa771a42
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-03-27 11:41:39 -0600

    add messages, display last message

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -1,12 +1,22 @@
-import React, { FC, useRef } from 'react'
+import React, { FC, useRef, useState } from 'react'
+import type { ChatCompletionRequestMessage as Message } from 'openai'
 
 const Game: FC = () => {
     const inputRef = useRef<HTMLInputElement>(null)
     const outputRef = useRef<HTMLParagraphElement>(null)
+    const [messages, setMessages] = useState<Array<Message>>([])
+
+    const getLastResponse = (messages: Array<Message>): string => {
+        const responses = messages.filter(msg => msg.role === 'assistant')
+        if (responses.length === 0) {
+            return ''
+        }
+        return responses[responses.length - 1].content
+    }
 
     return (
         <section>
-            <p ref={outputRef}></p>
+            <p ref={outputRef}>{getLastResponse(messages)}</p>
             <input ref={inputRef} type="text" />
         </section>
     )
