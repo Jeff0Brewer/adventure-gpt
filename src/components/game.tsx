2f30389781edc63da0ad26915b65d4ef0764543c
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-03-27 12:57:10 -0600

    add user response functionality

--- src/components/game.tsx
+++ src/components/game.tsx
@@ -5,43 +5,60 @@ import { postBody } from '@/lib/fetch'
 import styles from '@/styles/Game.module.css'
 
 const Game: FC = () => {
-    const inputRef = useRef<HTMLInputElement>(null)
-    const outputRef = useRef<HTMLParagraphElement>(null)
-    const [messages, setMessages] = useState<Array<Message>>([
-        { role: 'user', content: PROMPT }
-    ])
+    const [messages, setMessages] = useState<Array<Message>>([])
+    const [response, setResponse] = useState<string>('')
 
     useEffect(() => {
-        getCompletion(messages)
+        getCompletion({ role: 'user', content: PROMPT })
     }, [])
 
-    const getCompletion = async (messages: Array<Message>): Promise<void> => {
-        const completion = await fetch('/api/chat-complete', postBody({ messages }))
+    // set message state from new user message
+    const getCompletion = async (message: Message): Promise<void> => {
+        const newMessages = [...messages, message]
+        const completion = await fetch('/api/chat-complete', postBody({ messages: newMessages }))
         if (completion.status === 200) {
-            const { message } = await completion.json()
-            setMessages([
-                ...messages,
-                { role: 'assistant', content: message }
-            ])
+            const { message: content } = await completion.json()
+            setMessages([...newMessages, { role: 'assistant', content }])
+            setResponse(content)
         } else {
             const { message } = await completion.json()
             console.error(`Chat complete error: ${message}`)
         }
     }
 
-    const getLastResponse = (messages: Array<Message>): string => {
-        const responses = messages.filter(msg => msg.role === 'assistant')
-        if (responses.length === 0) {
-            return ''
+    return (
+        <section className={styles.chat}>
+            <p className={styles.output}>{response}</p>
+            <GameInput getCompletion={getCompletion} />
+        </section>
+    )
+}
+
+type GameInputProps = {
+    getCompletion: (message: Message) => void
+}
+
+const GameInput: FC<GameInputProps> = props => {
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    const sendMessage = (): void => {
+        if (!inputRef.current) return
+        const content = inputRef.current.value
+        inputRef.current.value = ''
+        props.getCompletion({ role: 'user', content })
+    }
+
+    const keySend = (e: React.KeyboardEvent<HTMLInputElement>) => {
+        if (e.key === 'Enter') {
+            sendMessage()
         }
-        return responses[responses.length - 1].content
     }
 
     return (
-        <section className={styles.chat}>
-            <p className={styles.output} ref={outputRef}>{getLastResponse(messages)}</p>
-            <input className={styles.input} ref={inputRef} type="text" />
-        </section>
+        <div className={styles.input}>
+            <input className={styles.text} ref={inputRef} type="text" onKeyDown={keySend} />
+            <button className={styles.send} onClick={sendMessage}>send</button>
+        </div>
     )
 }
 
