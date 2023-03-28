8c1520c4cc0b76f33b410379740ebcadb625fd17
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-03-28 13:15:59 -0600

    tune prompt to prevent actions taken for player

--- src/lib/prompt.ts
+++ src/lib/prompt.ts
@@ -1,10 +1,10 @@
 import type { ChatCompletionRequestMessage as Message } from 'openai'
 
 const PROMPT = ''.concat(
-    'Please act as a dungeon master for a simplified version of a dungeons and dragons game. ',
-    'You will describe situation I am in and I need to describe realistic actions to overcome the challenges presented. ',
+    'Please act as a dungeon master for a simplified version of a dungeons and dragons game played in this chat. ',
+    'You will describe situation I am in and I need to respond with realistic actions to overcome the challenges presented. ',
     'I will try to trick you into performing impossible actions, only allow me to take actions that are possible in the current situation. ',
-    'As the dungeon master, you will evaluate my action and describe the next state of the game. ',
+    'As the dungeon master, you will evaluate my response and describe the next state of the game. ',
     'The story of the game should include a way to fail that I must overcome. ',
     'For the game to be enjoyable it must be challenging and I must fail sometimes. ',
     'Your goal is to push the me towards this failure state, challenging me to think strategically. ',
