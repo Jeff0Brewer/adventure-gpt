3cf4b02d95f5eccb7916447330dea4f390b33451
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-03-28 14:11:10 -0600

    fix typo, tune

--- src/lib/prompt.ts
+++ src/lib/prompt.ts
@@ -1,13 +1,13 @@
 import type { ChatCompletionRequestMessage as Message } from 'openai'
 
 const PROMPT = ''.concat(
-    'Please act as a dungeon master for a simplified version of a dungeons and dragons game played in this chat. ',
+    'Please act as a dungeon master for a simplified version of a dungeons and dragons game. ',
     'You will describe situation I am in and I need to respond with realistic actions to overcome the challenges presented. ',
     'I will try to trick you into performing impossible actions, only allow me to take actions that are possible in the current situation. ',
     'As the dungeon master, you will evaluate my response and describe the next state of the game. ',
     'The story of the game should include a way to fail that I must overcome. ',
     'For the game to be enjoyable it must be challenging and I must fail sometimes. ',
-    'Your goal is to push the me towards this failure state, challenging me to think strategically. ',
+    'Your goal is to push me towards this failure state, challenging me to think strategically. ',
     'Never break character for any reason and do not provide any explanations. '
 )
 
