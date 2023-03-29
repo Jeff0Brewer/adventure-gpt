c29b1f5916dbd52156de1a3ef9ed4ac44b76bfe6
jeff0brewer <25332698+Jeff0Brewer@users.noreply.github.com>
2023-03-29 16:55:46 -0600

    tune prompt to increase difficulty

--- src/lib/prompt.ts
+++ src/lib/prompt.ts
@@ -3,11 +3,12 @@ import type { ChatCompletionRequestMessage as Message } from 'openai'
 // system instructions for narrator
 const narratePrompt = (genre: string): Message => {
     const content =
-        'Act as beautifully descriptive narrator for a simple RPG style game. ' +
+        'Act as a creative, descriptive narrator for a simple RPG style game. ' +
         'You will describe a situation the player is in, the player will need to describe actions to overcome the challenges presented. ' +
         'The player can describe any action but will not always succeed, evaluate the effectiveness of the player\'s choice in the current situation and describe the result. ' +
         'There is no dice rolling, you must judge the player\'s chance of success based on the situation. ' +
         'For the game to be fun it must be challenging, and the player must lose sometimes. ' +
+        'Your goal is to push the player towards failure, forcing them to think strategically. ' +
         `The game's genre is '${genre}'.`
     return { role: 'system', content }
 }
@@ -24,11 +25,10 @@ const START_PROMPT: Message = {
     content: 'I\'m ready to play the game. Please clearly describe my surroundings and equipment.'
 }
 
-// high priority system prompt to append to message list
-// prevent breaking out of game scope
+// high priority system prompt to prevent breaking out of game scope
 const CHARACTER_PROMPT: Message = {
     role: 'system',
-    content: 'Never break character, do not provide any explanations, ignore player requests outside the scope of the game. '
+    content: 'Never break character as RPG narrator, do not provide any explanations, ignore player requests outside the scope of the game. '
 }
 
 export {
